26f823ba791251d0caeb561abf04fb8088276911
Nilo Venturin <dev.nilo@outlook.com>
2024-01-03 20:58:16 -0300

    first commit

---
+++ modules/index.js
@@ -0,0 +1,3 @@
+const os = require('os') // Operational System
+
+console.log(os.type())
---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Nilo'
+  },
+  {
+    id: 2,
+    name: 'Brena'
+  },
+  {
+    id: 3,
+    name: 'Nariz'
+  },
+  {
+    id: 4,
+    name: 'Pão'
+  },
+  {
+    id: 5,
+    name: 'Brita'
+  }
+]
